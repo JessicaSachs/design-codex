b6a6696fb658cc8fc2e30c3f531e189da0369760
Jessica Sachs <jess@jessicasachs.io>
2022-03-31 22:51:25 -0400

    example configuration for Cy CT

---
+++ packages/codex/src/components/typeahead-search/TypeaheadSearch.cy.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef */
+import TypeaheadSearchWikipedia from './TypeaheadSearchWikipedia.vue';
+import { mount } from '@cypress/vue';
+
+const inputSelector = '[placeholder="Search Wikipedia"]';
+
+beforeEach( () => {
+	mount( TypeaheadSearchWikipedia );
+} );
+
+it( 'has an input that says Search Wikipedia', () => {
+	cy.get( inputSelector ).should( 'be.visible' );
+} );
+
+it( 'shows the Search button when you click on the input', () => {
+	cy.findByRole( 'button', { name: 'Search' } )
+		.should( 'not.be.visible' )
+		.get( inputSelector )
+		.click()
+		.get( 'body' )
+		.findByRole( 'button', { name: 'Search' } )
+		.should( 'be.visible' );
+} );
+
+it.only( 'fires a network request when typing with the queried values', () => {
+//  Request URL: https://en.wikipedia.org/w/rest.php/v1/search/title?q=hello%20hkit&limit=10&
+	cy.intercept( '*/search/title?*' ).as( 'searchSpy' );
+	cy.get( inputSelector )
+		.type( 'Hello Kitty' )
+		.get( '@searchSpy' );
+} );
